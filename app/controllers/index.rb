5bedabbd6583e74df0db82dd7d97c1264e59a2f0
eadonj <eadonj@gmail.com>
2013-06-06 17:49:40 -0600

    Update index.rb

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -21,6 +21,12 @@ post '/sessions' do
   end
 end
 
+#post '/sessions' do
+#  @user = User.authenticate(params[:email], params[:password])
+#  session[:user_id] = @user.id if @user
+#  redirect '/'
+#end
+
 delete '/sessions/:id' do
   session.clear
   ""
